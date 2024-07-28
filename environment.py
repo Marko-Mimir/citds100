8281bd81598733ba7da5adb14624dc7af8a4846b
Marko-Mimir <95181761+Marko-Mimir@users.noreply.github.com>
2024-07-28 17:29:39 -0400

    move

--- environment.py
+++ environment.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
+import copy
 
 class environment:
 
@@ -8,10 +9,14 @@ class environment:
         maxVert = 0;
         maxHor = 0;
         index = []
-        eax = 0
-        ebx = 0
+        eax = None
+        ebx = None
+        line = 0
         code = ""
         env = None;
+        get = None;
+        ret = None
+        codeParser : dict;
 
         def __str__(self):
             eax = str(self.eax)
@@ -22,95 +27,145 @@ class environment:
         def __init__(self, env, index, maxVert, maxHor, outputDir = None):
             self.env = env
             self.index = index
-            # index[0] = collum, index[1] = row
-            self.name = str(index[0])+"-"+str(index[1])
+            # index[0] = horiz, index[1] = vert
+            self.name = str(index[1])+"-"+str(index[0])
             self.maxVert = maxVert
             self.maxHor = maxHor
-            if outputDir != null:
+            if outputDir != None:
                 self.outputDir = outputDir
 
         def storeCode(self, code):
             self.code = code
 
-        def parseCode(self):
-            split = self.code.split("\n")
-            for x in split:
-                match x.split(" ")[0]:
-                    case "ADD":
-                        self.add()
-                    case "MOVE":
-                        result = self.move(x[1].lower())
-                        if result == "err":
-                            return "Error at line ["+x+"]. Incorrect direction, syntax= MOVE [UP/LEFT/DOWN/RIGHT]"
-                        if result == "NON EXIST":
-                            if outputDir == x[1].lower():
-                                env.output(self.eax)
-                        else:
-                            self.env.findGridSquare(result[0], result[1]).retreive(self.eax)
-                        self.eax = 0;
-                        print('worked somehow')
-                    case "SUB":
-                        self.sub()
-                    case "STORE":
-                        self.store()
-                    case "SWP":
-                        self.swap()
+        def wait(self, line):
             return "OK"
-                        
-        def swap(self):
+        def recieve(self, line):
+            print(f"WORK @ {self.name}: RECIEVING {self.ret}")
+            if self.eax == None:
+                if self.ret != None:
+                    self.eax = self.ret
+                    self.ret = None
+                    return "OK"
+        
+        def parseCode(self) -> str:
+            if self.code == []:
+                return "OK"
+            split = self.code.split("\n")
+            
+            if split[self.line].split(" ")[0] not in self.codeParser.keys():
+                return f"error at line [{self.line}] with value [{split[self.line]}]"
+            
+            res = self.codeParser[split[self.line].split(" ")[0]](self, split[self.line])
+            if res == "OK":
+                self.line += 1
+                if self.line > len(split)-1:
+                    self.line = 0
+            elif res:
+                return res
+            return "OK"
+        
+        def move(self, line : str): # THIS IS A BIGG MESS :(((
+            move = copy.deepcopy(self.index)
+            exist = True
+            direction = line.split(" ")[1].lower()
+            match direction:
+                case "up" | "u":
+                    move[1] = move[1]+1
+                    if move[1] > self.maxVert:
+                        exist = False
+                case "down" | "d":
+                    move[1] = move[1]-1
+                    if move[1] < 0:
+                        exist = False
+                case "left" | "l":
+                    move[0] = move[0]-1
+                    if move[0] < 0:
+                        exist = False
+                case "right" | "r":
+                    move[0] = move[0]+1
+                    if move[0] > self.maxHor:
+                        exist = False
+                case "_":
+                    return "err"
+            if exist == False:
+                if self.outputDir == direction:
+                    self.env.placeOut(self.eax)
+                    self.eax = None
+                    return "OK"
+            else:
+                print(f"{self.index} is moving in the {direction} direction to {move}")
+                res = self.env.findGridSquare(move[1], move[0]).retreive(self.eax)
+                if res:
+                    self.eax = None
+                    return "OK"
+                else:
+                    print("MOVE FAIL")
+                    return
+
+
+        
+        def swap(self, line):
             temp = self.eax
             self.eax = self.ebx
             self.ebx = temp
+            return "OK"
 
-        def store(self):
+        def store(self, line):
+            if self.eax == None:
+                return "OK"
             self.ebx = self.eax
+            self.eax = None
             print(self.ebx)
+            return "OK"
 
-        def sub(self):
+        def sub(self, line):
+            if self.eax == None:
+                return "OK"
+            if self.ebx == None:
+                self.ebx = 0
             self.ebx = self.ebx-self.eax
+            self.eax = None
             print(self.ebx)
+            return "OK"
 
-        def add(self):
+        def add(self, line):
+            if self.eax == None:
+                return "OK"
+            if self.ebx == None:
+                self.ebx = 0
             self.ebx = self.ebx+self.eax
+            self.eax = None
             print(self.ebx)
+            return "OK"
 
-        def retreive(self, val):
-            self.eax = val
+        def retreive(self, val) -> bool:
+            if self.ret == None:
+                self.ret = val
+                return True
+            return False
         
-        def move(self, direction):
-            move = self.index
-            match direction:
-                case "up" | "u":
-                    move[1] = self.index[1]+1
-                    if move[1] > self.maxVert:
-                        return "NON EXIST"
-                case "down" | "d":
-                    move[1] = self.index[1]-1
-                    if move[1] < 0:
-                        return "NON EXIST"
-                case "left" | "l":
-                    move[0] = self.index[0]-1
-                    if move[0] < 0:
-                        return "NON EXIST"
-                case "right" | "r":
-                    move[0] = self.index[0]+1
-                    if move[0] > self.maxHor:
-                        return "NON EXIST"
-                case "_":
-                    return "err"
-            return move
+        codeParser = {"WAIT":wait, "MOVE":move, "GET":recieve, "ADD":add, "SUB":sub, "SWP":swap,"STORE":store}
 
     grid = [];
     output = [];
-    ecx = 0;
+    expectedOutput = [];
+    inputList = [];
+    inputSquare : gridsquare = None
+    inputSpace = 0
+    outputSpace = 0
+    stepInt = 0;
+    stepLimit = 256
 
-    def __init__(self, rows, collums):
-        for x in range(collums):
+    def __init__(self, rows, collums, input, output):
+        self.grid = [] #FOR SOME REASON IT SAVES I HAVE NO IDEA WHY :(
+        self.inputSpace = input
+        self.outputSpace = output
+        for y in range(collums):
             collum = []
-            for y in range(rows):
-                square = self.gridsquare(self, [x,y], rows-1, collums-1)
+            for x in range(rows):
+                print(f"{x}=x, {y}=y")
+                square = self.gridsquare(self, [x,y], collums-1, rows-1)
                 collum.append(square)
-                test = str(x)+"="
             self.grid.append(collum)
 
         #make grid image
@@ -122,6 +177,8 @@ class environment:
 
         draw = ImageDraw.Draw(gridImage)
         draw.font = ImageFont.truetype("FreeMono.ttf")
+
+        #draw grid
         for x in range(rows):
             if x != 0:
                 lineCords = (x*cellSize, 0, x*cellSize, collums*cellSize)
@@ -131,25 +188,71 @@ class environment:
                 lineCords = (0, x*cellSize, rows*cellSize, x*cellSize)
                 draw.line(lineCords, width=3, fill=(100, 100, 100))
         
+        #add numbers
         itterator = 0
         for row in range(collums):
             for collom in range(rows):
                 itterator = itterator + 1
                 xy = ((collom*cellSize)+20, (row*cellSize)+20)
                 draw.text(xy, str(itterator), fill=(0,0,0), font_size=16)
-
+        #Add border
         Image.Image.paste(mainImage, gridImage, (50,50))
+
+        #input/output text
+        draw = ImageDraw.Draw(mainImage)
+        draw.font = ImageFont.truetype("Arial.ttf")
+        xy = ((input*cellSize)+50+(cellSize/4), 5)
+        draw.text(xy, "Input", fill=(255,0,0), font_size=32)
+        xy = ((output*cellSize)+50+(cellSize/4), (collums*cellSize)+55)
+        draw.text(xy, "Output", fill=(255,0,0), font_size=32)
+
+        #set grid output via code
+        self.grid[len(self.grid)-1][output].outputDir = "down"
+        #set inputGrid
+        self.inputSquare = self.grid[0][input]
+
         mainImage.save("image.png", "PNG")
-    
+        print(self.grid)
+
+
     #rows = horizonatlly colloms = top and down
     def findGridSquare(self, collom, row):
         return self.grid[collom][row]
 
-    def output(self, output):
-        self.output.append(output)
-    
-test = environment(5, 3)
-test.findGridSquare(0,0).storeCode("MOVE DOWN")
-test.findGridSquare(0,0).parseCode()
+    def select(self, id):
+        right = id
+        max = len(self.grid[0])
+        down = 0
+        while right >max:
+            right -= max
+            down += 1
+        return(self.grid[down][right-1])
 
+    def placeOut(self, output):
+        self.output.append(output)
+        print(f"Output Updated: {self.output}")
+    
+    def step(self) -> str:
+        #make sure its not impossible
+        if self.stepInt >= self.stepLimit:
+            return "ERROR: MEMORY ERROR. TRY FASTER APPROACH OR CURRENT APPROACH IS IMPOSSIBLE."
+        elif not self.inputSquare.code:
+            return "ERROR: IMPOSSIBLE. INPUT SQUARE HAS NO CODE."
+        elif not self.grid[len(self.grid)-1][self.outputSpace].code:
+            return "ERROR: IMPOSSIBLE. OUTPUT SQUARE HAS NO CODE."
+        if self.inputList:
+            result = self.inputSquare.retreive(self.inputList[0])
+            if result:
+                self.inputList.pop(0)
+        for x in self.grid:
+            for square in x:
+                res = square.parseCode()
+                if res != "OK":
+                    return f"ERROR: [{res}]"
+        if len(self.expectedOutput) == len(self.output):
+            return "DONE"
+        self.stepInt += 1
+        print(f"Grid is on step {self.stepInt}")
+        return "OK"
+        
 
--- main.py
+++ main.py
@@ -1,15 +1,274 @@
-import interactions
+import interactions, json, asyncio, copy
 from interactions import Client, Intents, listen
 from interactions.ext import prefixed_commands
-from interactions.api.events import MessageCreate
+from interactions.ext.prefixed_commands import prefixed_command, PrefixedContext
+from environment import environment
+
 
 bot = Client(token="ODUyOTMwMzAwMTIzMTUyMzk0.GEAAuR.7z2c0Kbs073jIK0zblLEvCYEvOTR1uGjJO9AsE",
 intents=Intents.new(default=True, guild_messages=True, message_content=True))
+env : environment = None
+gameMessage : interactions.Message = None
+selected : environment.gridsquare = None
+f = open('levels.json')
+data : dict= json.load(f)
+f.close()
+selectedMessage : interactions.Message = None
+isListening = False
+selectedLabel = 0
+currentLevelDat = None
 
-prefixed_commands.setup(bot)
+prefixed_commands.setup(bot, default_prefix=">")
 
-@listen(MessageCreate)
-async def on_message_create(event : MessageCreate):
-    print(event.message.content)
+@listen()
+async def on_startup():
+    print("Citds started")
+
+@listen()
+async def on_message_create(event : interactions.events.MessageCreate):
+    if event.message.author.bot:
+        return
+    if event.message.content.startswith(">"):
+        return;
+    if not isListening:
+        return;
+    global selectedMessage
+    if selectedMessage == None:
+        return
+    if event.message.channel.id != selectedMessage.channel.id:
+        return;
+    global selected
+    global selectedLabel
+    if selected == None:
+        return;
+    await event.message.delete()
+    selected.code = event.message.content
+    selected.line = 0
+    code = ""
+    itterator = 1
+    for x in selected.code.split("\n"):
+        code = code+f"{itterator}] {x}\n"
+        itterator+=1
+    await selectedMessage.edit(content=f"```CURRENTLY SELECTED NODE [{selectedLabel}]. CURRENT CODE:\n{code}```")
+
+
+
+@prefixed_command(name="test")
+async def on_test(ctx : PrefixedContext):
+    global gameMessage
+    await gameMessage.edit(file="test.png")
+
+
+@prefixed_command(name="kill")
+async def killGame(ctx : PrefixedContext):
+    global gameMessage
+    global selectedMessage
+    global isListening
+    global selected
+    await ctx.message.delete()
+    if gameMessage != None:
+        await gameMessage.delete()
+        await selectedMessage.delete()
+        selectedMessage = None
+        gameMessage = None
+        isListening = False
+        elected = None
+
+@prefixed_command(name="start")
+async def onStart(ctx : PrefixedContext, level= ""):
+    if level == "":
+        await ctx.send("This command needs a [LEVEL] Attribute. Syntax: >start [LevelId]")
+        return
+    global env
+    global gameMessage
+    global selectedMessage
+    await ctx.message.delete()
+    if gameMessage != None:
+        await gameMessage.delete()
+        gameMessage = None
+
+    if level not in data.keys():
+        await ctx.send(f"[Levels.json] does not have levelId: [{level}]")
+        return
+    global currentLevelDat
+    lDat = data[level]
+    currentLevelDat = copy.deepcopy(lDat)
+    env = None
+    env = environment(rows=lDat["rows"],collums=lDat["collums"], input=lDat["input"], output=lDat["output"])
+    env.inputList = lDat["inpList"]
+    env.expectedOutput = lDat["outList"]
+    emb = getGameEmbed()
+    actionRow: list[interactions.ActionRow] =[
+        interactions.ActionRow(
+            interactions.Button(
+                style=interactions.ButtonStyle.BLURPLE,
+                label="Step",
+                emoji=":arrow_forward:",
+                custom_id="step"
+            ),
+            interactions.Button(
+                style=interactions.ButtonStyle.GREEN,
+                label="Finish",
+                emoji=":track_next:",
+                custom_id="finish"
+            ),
+            interactions.Button(
+                style=interactions.ButtonStyle.RED,
+                label="Reset",
+                emoji=":arrows_counterclockwise:",
+                custom_id="reset"
+            )
+        )
+    ]
+    
+    gameMessage = await ctx.send(components=actionRow, embed=emb, file="image.png")
+    selectedMessage = await ctx.send("`CURRENTLY SELECTED NODE [none]. SELECT WITH >select [NODE ID]`")
+
+
+async def disableGame(ctx : PrefixedContext):
+    global gameMessage
+    global selectedMessage
+    global isListening
+    global selected
+    actionRow: list[interactions.ActionRow] =[
+        interactions.ActionRow(
+            interactions.Button(
+                style=interactions.ButtonStyle.BLURPLE,
+                label="Step",
+                emoji=":arrow_forward:",
+                custom_id="step",
+                disabled=True
+            ),
+            interactions.Button(
+                style=interactions.ButtonStyle.GREEN,
+                label="Finish",
+                emoji=":track_next:",
+                custom_id="finish",
+                disabled=True
+            ),
+            interactions.Button(
+                style=interactions.ButtonStyle.RED,
+                label="reset",
+                emoji=":arrows_counterclockwise:",
+                custom_id="reset",
+                disabled=True
+            )
+        )
+    ]
+    isListening = False
+    selected = None
+    await gameMessage.edit(components=actionRow)
+    await selectedMessage.delete()
+    env = None
+
+@interactions.component_callback("reset")
+async def resetGame(ctx : interactions.ComponentContext):
+    global env
+    global currentLevelDat
+    global gameMessage
+    env.stepInt = 0
+    env.inputList = currentLevelDat["inpList"]
+    print(f"Resetting... inputList= {env.inputList}")
+    env.output = []
+    await gameMessage.edit(embed=getGameEmbed())
+    msg : interactions.Message = await ctx.send("Game reset")
+    await asyncio.sleep(1)
+    await msg.delete()
+
+def getGameEmbed():
+    global currentLevelDat
+    emb = interactions.Embed(title=currentLevelDat["name"], color=(0,255,0), fields=[
+        interactions.EmbedField("Input", surroundIndex("**",0, env.inputList)),
+        interactions.EmbedField("Expected Output", str(env.expectedOutput), True),
+        interactions.EmbedField("Current Output", str(env.output)),
+    ],footer=interactions.EmbedFooter(f"Node is on step {env.stepInt}"))
+    return emb
+
+
+
+@interactions.component_callback("step")
+async def nextStep(ctx : interactions.ComponentContext):
+    global env
+    temp : interactions.Message = None
+    if env.output:
+        temp =await ctx.send("SUGGESTION: After verifying the [**OUTPUT**] matches [**EXPECTED OUTPUT**], press [**FINISH**] to speed up the repair process.")
+    res = env.step()
+    if res == "DONE":
+        await ctx.send("NODE REPAIRED")
+        await disableGame(ctx)
+    elif res != "OK":
+        await ctx.send(res)
+        return
+    await gameMessage.edit(embed=getGameEmbed())
+    tmp : interactions.Message = None
+    if temp == None:
+        tmp = await ctx.send(content="Node Stepped.")
+    await asyncio.sleep(.5)
+    if tmp != None:
+        await tmp.delete()
+    if temp != None:
+        await asyncio.sleep(4)
+        await temp.delete()
+
+@interactions.component_callback("finish")
+async def finishGame(ctx : interactions.ComponentContext):
+    global env
+    res = "OK"
+    while res == "OK":
+        res = env.step()
+    print(res)
+    if res == "DONE":
+        await ctx.send("NODE REPAIRED")
+        await disableGame(ctx)
+    else:
+        await ctx.send(res)
+    await gameMessage.edit(embed=getGameEmbed())
+
+def surroundIndex(surround, index, array) -> str:
+    if index > len(array):
+        return "index is out of Array."
+    itterator = 0
+    fString = "["
+    for x in array:
+        comma = ", "
+        if itterator == 0:
+            comma = ""
+        if itterator == index:
+            fString = fString+comma+surround+str(x)+surround
+        else:
+            fString = fString +comma+str(x)
+        itterator += 1;
+    fString = fString+"]"
+    return fString
+
+@prefixed_command(name="select")
+async def onSelect(ctx : PrefixedContext, num):
+    global selected
+    global env
+    if env == None:
+        await ctx.send("ERROR: ENVIORMENT ISN'T SET. PLEASE START A NODE REPAIR")
+        return
+    await ctx.message.delete()
+    square : environment.gridsquare = env.select(int(num))
+    selected = square
+    code = ""
+    itterator = 1
+    global selectedLabel
+    global isListening
+    global selectedMessage
+    selectedLabel = num
+    for x in selected.code.split("\n"):
+        code = code+f"{itterator}] {x}\n"
+        itterator+=1
+    await selectedMessage.edit(content=f"```CURRENTLY SELECTED NODE [{num}]. CURRENT CODE:\n{code}```")
+    isListening = True
+
+@prefixed_command(name= "code")
+async def getCode(ctx : PrefixedContext, arg = None):
+    global selected
+    await ctx.send(f"Current code: {selected.code}")
+    if arg != None:
+        selected.code = arg
+        await ctx.send(f"New code: {selected.code}")
 
 bot.start()
